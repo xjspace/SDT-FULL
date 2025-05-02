03a8f5f3ea26d25ecb2b6687ccd5bde43e087b7a
hunter <support@xjspace.net>
2025-05-02 11:08:32 +0800

    删除所有文件

--- podcast_agent/podcast_agent/__init__.py
+++
@@ -1,2 +0,0 @@
-# 初始化podcast_agent包
-from .core.processing import process_podcast_url
--- podcast_agent/podcast_agent/core/processing.py
+++
@@ -1,5 +0,0 @@
-def process_podcast(url: str):
-    """完整的播客处理流程"""
-    # 添加实际业务逻辑
-    processor = AudioProcessor()
-    return processor.process(url)
